9498b377399e119dd83652b34f67e5d1fb3457e9
joaquin.gajardo@empa.ch <jagajardo@uc.cl>
2022-12-16 11:08:05 +0100

    weighted loss fn

--- scripts/models.py
+++ scripts/models.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--max_epochs", type=int, default=1000)
     parser.add_argument("--patience", type=int, default=10)
+    parser.add_argument("--weighted_loss_fn", default=False, action="store_true")
 
     model_args = STR2MODEL["land_cover"].add_model_specific_args(parser).parse_args()
     model = STR2MODEL["land_cover"](model_args)
