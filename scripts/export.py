5741af5674effcdabbb0e04aff9afa07c1f793ec
Joaquin-Gajardo <joaquin.gajardocastillo@gmail.com>
2023-07-24 18:34:50 +0200

    working gdrive exporter standard google oauth

--- scripts/export.py
+++ scripts/export.py
@@ -52,28 +52,18 @@ def export_region():
         metres_per_polygon=None,
     )
 
+
 def export_gdrive_nigeria():
     exporter = GDriveExporter(Path("../data"), dataset=NigeriaSentinelExporter.dataset)
-    exporter.export_with_gdown()
+    exporter.export()
+
 
 def export_gdrive_nigeria_S1(api_key=None):
     # We need to put the credentials.json file in th output folder (get it from the google clound console). 
-    # A token.pickle file will be generated on the same folder after authentification.
+    # A token.json file will be generated on the same folder after authentification.
     exporter = GDriveExporter(Path('/media/Elements-12TB/satellite_images/nigeria'), dataset='nigeria-full-country-2020')
-    path = Path('../gdrive_test_eo_data_files.csv')
-    file_info = []
-    if path.exists():
-        print('Loading csv files with names')
-        df = pd.read_csv(path)
-        file_info = df.to_dict('records')
-    else:
-        print('Listing files on gdrive...')
-        file_info = exporter.list_files_in_folder(folder_name='test_eo_data')
-        df = pd.DataFrame(file_info)
-        df.to_csv(path, index=False)
-    
-    # You can generate an API key on the google cloud console
-    exporter.export_with_api_key(file_info, api_key)
+    exporter.export(folder_name='test_eo_data')
+
 
 def cancel_tasks():
     cancel_all_tasks()
--- src/exporters/gdrive.py
+++ src/exporters/gdrive.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 import pickle
 import os
+import io
 
+import pandas as pd
 import gdown
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
 
 from typing import Dict, List, Optional
 
@@ -15,7 +20,8 @@ class GDriveExporter:
     An exporter to download data from Google Drive
     """
 
-    scopes = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
+    #scopes = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
+    scopes = ["https://www.googleapis.com/auth/drive"]
 
     def __init__(self, data_folder: Path, dataset: str) -> None:
         self.data_folder = data_folder
@@ -24,7 +30,8 @@ class GDriveExporter:
         self.output_folder = self.raw_folder / self.dataset
         self.output_folder.mkdir(parents=True, exist_ok=True) # we will only save the token here
 
-        assert (self.output_folder / "credentials.json").exists(), (
+        credentials_path = self.output_folder / "credentials.json"
+        assert credentials_path.exists(), (
             f"Enable the google drive API at this link: "
             f"https://developers.google.com/drive/api/v3/quickstart/python "
             f"to use this class. Save the credentials.json at {self.output_folder}"
@@ -32,34 +39,46 @@ class GDriveExporter:
 
         # https://developers.google.com/drive/api/v3/quickstart/python
         creds = None
-        token_path = self.output_folder / "token.pickle"
+        token_path = self.output_folder / "token.json"
         if token_path.exists():
-            with token_path.open("rb") as f:
-                creds = pickle.load(f)
-
+            creds = Credentials.from_authorized_user_file(token_path, self.scopes)
+        # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    self.output_folder / "credentials.json", self.scopes
-                )
+                flow = InstalledAppFlow.from_client_secrets_file(credentials_path, self.scopes)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with (self.output_folder / "token.pickle").open("wb") as token:
-                pickle.dump(creds, token)
+            with open(token_path, 'w') as token:
+                token.write(creds.to_json())
 
         self.service = build("drive", "v3", credentials=creds)
 
-    def list_files_in_folder(self, folder_name: str, max_downloads: Optional[int] = None) -> None:
+    def list_files_in_folder(self, folder_name: str, file_path: Path=None, max_downloads: Optional[int] = None) -> None:
         r"""
-        Download all tiffs from a Google Drive folder.
+        List all tiffs from a Google Drive folder or load file list from csv file..
 
-        :param folder_name: will download all tiffs in this folder.
+        :param folder_name: list all tiffs in this google drive folder. Write list to csv file.
+        :param file_path: Optional path where to write csv file or to read from, if not provided it will write it to 
         :param max_downloads: The max number of downloads. If None, all tiff files containing
             region_name are downloaded
         """
+        file_info = []
+
+        if file_path is None:
+            file_path = self.output_folder / f"gdrive_{folder_name}_files.csv" 
+
+        if file_path.exists():
+            print(f'Loading of files names and ids from csv at {file_path}')
+            df = pd.read_csv(file_path)
+            file_info = df.to_dict('records')
+            return file_info
+
+        ## If file doesn't exist list all files in gdrive target folder and save to disk
+
         # Get parent folder ID
+        print('Listing files on gdrive...')
         query = f'(fullText contains "{folder_name}") and (mimeType = "application/vnd.google-apps.folder")'
 
         folder_info: List[Dict] = [] 
@@ -111,33 +130,57 @@ class GDriveExporter:
             next_page = results.get("nextPageToken", None)
 
         print(f"There are {len(file_info)} tiff files from {folder_name} folder in Google Drive")
-        
 
-        # for idx, individual_file in enumerate(file_info):
-        #     if (max_downloads is not None) and (idx >= max_downloads):
-        #         return None
-
-        #     print(f"Downloading {individual_file['name']}")
-
-        #     url = f"https://drive.google.com/uc?id={individual_file['id']}"
-
-        #     download_path = (
-        #         self.output_folder / individual_file["name"]
-        #     )
-            
-        #     if download_path.exists():
-        #         print(f"File already exists! Skipping")
-        #         continue
-
-        #     data = self.service.files().get_media(fileId=individual_file['id']).execute() # https://stackoverflow.com/questions/65053558/google-drive-api-v3-files-export-method-throws-a-403-error-export-only-support
-        #     if data:
-        #         with open(download_path, 'wb') as f:
-        #             f.write(data)
-
-        # print('Program finished')
+        # Write files to disk
+        print(f'Writing file names and ids to {file_path}')
+        df = pd.DataFrame(file_info)
+        df.to_csv(file_path, index=False)
 
         return file_info
-    
+
+    def export(self, folder_name: str, reversed_order: bool=False) -> None:
+        
+        file_info = self.list_files_in_folder(folder_name)
+        if reversed_order:
+            file_info = reversed(file_info)
+
+        for i, file_metadata in enumerate(file_info):
+            
+            file_id = file_metadata['id']
+            file_name = file_metadata['name'].split('/')[-1]
+            output_path = self.output_folder / file_name
+
+            if output_path.exists():
+                print(f"File {file_name} already exists! Skipping")
+                continue
+            else:
+                print(f"Downloading file {i}/{len(file_info)} {file_name} with id {file_id} from drive into {output_path}")
+                try:
+                    # Read file by chunks
+                    request = self.service.files().get_media(fileId=file_id)
+                    data = request.execute()
+                    if data:
+                        with open(output_path, 'wb') as f:
+                            print(f'Writing file to {output_path}')
+                            f.write(data)    
+
+                    # file = io.BytesIO()
+                    # downloader = MediaIoBaseDownload(file, request)
+                    # done = False
+                    # while done is False:
+                    #     status, done = downloader.next_chunk()
+                    #     print(F'Download {int(status.progress() * 100)}.')
+                    
+                    # # Write to file
+                    # with io.open(output_path, 'wb') as f:
+                    #     print(f'Writing file to {output_path}')
+                    #     file.seek(0)
+                    #     f.write(file.read())            
+        
+                except HttpError as error:
+                    print(F'An error occurred: {error}')
+                    break
+
     def export_with_gdown(self, file_info: List[Dict]) -> None:
 
         if file_info is not None:
